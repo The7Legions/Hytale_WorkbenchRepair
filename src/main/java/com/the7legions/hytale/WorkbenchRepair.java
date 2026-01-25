fb21191ff7ad7035c279c5d9e9cf4b5a22063a8a
Richard MacAleese <macaleese.1@gmail.com>
2026-01-25 01:20:01 -0500

    Added configuration file with settings for blacklist and allowing t1 workbench.

--- src/main/java/com/the7legions/hytale/WorkbenchRepair.java
+++ src/main/java/com/the7legions/hytale/WorkbenchRepair.java
@@ -3,18 +3,28 @@ package com.the7legions.hytale;
 import com.hypixel.hytale.server.core.event.events.ecs.UseBlockEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.util.Config;
 import com.the7legions.hytale.events.ecs.PlayerUseBlockEventSystem;
 
 import javax.annotation.Nonnull;
 
 public class WorkbenchRepair extends JavaPlugin {
 
+    private final Config<WorkbenchRepairConfig> config;
+
     public WorkbenchRepair(@Nonnull JavaPluginInit init) {
         super(init);
+
+        config = this.withConfig("WorkbenchRepairConfig", WorkbenchRepairConfig.CODEC);
+    }
+
+    public Config<WorkbenchRepairConfig> getConfig() {
+        return this.config;
     }
 
     @Override
     protected void setup() {
-        this.getEntityStoreRegistry().registerSystem(new PlayerUseBlockEventSystem(UseBlockEvent.Pre.class));
+        this.getEntityStoreRegistry().registerSystem(new PlayerUseBlockEventSystem(UseBlockEvent.Pre.class, this));
+        config.save();
     }
 }
